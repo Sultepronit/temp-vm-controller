b704eb87285e73be2ffcb0c1283db9ce3b4545f5
Sultepronit <sultepronit@gmail.com>
2024-03-03 17:09:59 +0200

    added error/warnings output(?)

--- get-file.php
+++ get-file.php
@@ -3,6 +3,19 @@ declare(strict_types=1);
 
 header('Access-Control-Allow-Origin: *');
 
+function customErrorHandler($errno, $errstr, $errfile, $errline) {
+    if ($errno == E_WARNING) {
+        // Handle warnings
+        echo "Warning: $errstr in $errfile on line $errline\n";
+    } else {
+        // Handle other types of errors
+        echo "Error: $errstr in $errfile on line $errline\n";
+    }
+}
+
+// Set the custom error handler
+set_error_handler('customErrorHandler');
+
 $postJson = file_get_contents('php://input');
 $postData = json_decode($postJson, true);
 print_r($postData);

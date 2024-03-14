64af7d809d963f7fc00d94b0436f19c3a77978eb
Sultepronit <sultepronit@gmail.com>
2024-03-14 09:26:31 +0200

    created vue version, added the monaco

---
+++ ide-0/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+console.log('Here we go!');
+import { ref, onMounted } from 'vue';
+
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+console.log(monaco);
+
+const editor = ref(null);
+console.log(editor);
+
+let monacoEditor = null;
+onMounted(() => {
+    console.log(editor);
+    console.log(editor.value);
+
+    const params = {
+        wordWrap: 'on',
+        theme: 'vs-dark',
+        // theme: 'hc-black',
+        fontSize: '20',
+        // language: 'html'
+    };
+    monacoEditor = monaco.editor.create(editor.value, params);
+});
+</script>
+
+<template>
+    <h1>Go!</h1>
+    <div ref="editor" id="editor"></div>
+</template>
+
+<style scoped>
+#editor {
+    height: 500px;
+}
+</style>

61c7a01a4da5f748865ace4eea757405da42812f
Sultepronit <sultepronit@gmail.com>
2024-03-14 09:44:49 +0200

    started developting filesystem representaion

--- ide-0/src/App.vue
+++ ide-0/src/App.vue
@@ -1,36 +1,24 @@
 <script setup>
-console.log('Here we go!');
-import { ref, onMounted } from 'vue';
+import sendRequest from './js/api.js';
+async function getDirCont(dirName) {
+    const postData = {
+        action: 'getFileTree',
+        baseDir: dirName
+    }
+    console.log(postData);
 
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-console.log(monaco);
-
-const editor = ref(null);
-console.log(editor);
-
-let monacoEditor = null;
-onMounted(() => {
-    console.log(editor);
-    console.log(editor.value);
-
-    const params = {
-        wordWrap: 'on',
-        theme: 'vs-dark',
-        // theme: 'hc-black',
-        fontSize: '20',
-        // language: 'html'
-    };
-    monacoEditor = monaco.editor.create(editor.value, params);
-});
+    const resp = await sendRequest(postData);
+    const dirContObject = JSON.parse(resp);
+ 
+    console.log(dirContObject);
+}
+getDirCont('..');
 </script>
 
 <template>
-    <h1>Go!</h1>
-    <div ref="editor" id="editor"></div>
+    <h1>Go</h1>
 </template>
 
 <style scoped>
-#editor {
-    height: 500px;
-}
+
 </style>
---
+++ ide-0/src/js/api.js
@@ -0,0 +1,16 @@
+const url = '../main2.php';
+
+const password = 'pass';
+
+async function sendRequest(data) {
+    data.password = password;
+    const resp = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    });
+    const result = await resp.text();
+    console.log(result);
+    return result;
+}
+
+export default sendRequest;
